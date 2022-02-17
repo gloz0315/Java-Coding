d000b3f874dc45d1a4dc6835988b34b5251729db
Byeong Soo Lee <80665963+gloz0315@users.noreply.github.com>
2022-02-17 11:50:27 +0900

    Create Star.java

---
+++ Do it!/Star.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Spria {
+    static void spira(int n){
+        for(int i = 1; i < n+1; i++){
+            for(int j = 0; j < 5-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < 2*(i-1)+1; j++)
+                System.out.print("*");
+
+        System.out.println();
+        }
+    }
+
+    public static void main(String args[]){
+        Scanner stdIn = new Scanner(System.in);
+
+        int n;
+        System.out.print("숫자를 입력하세요: "); n = stdIn.nextInt();
+        spira(n);
+    }
+}
