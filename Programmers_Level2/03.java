19a2b783950eaf7b1c8d530d5897ec428756a4c1
Byeong Soo Lee <80665963+gloz0315@users.noreply.github.com>
2022-03-04 22:58:36 +0900

    Create 03.java

---
+++ Programmers_Level2/03.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < phone_book.length; i++){
+            map.put(phone_book[i],i); // key-value값 
+        }
+        
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 0; j < phone_book[i].length(); j++){
+                if(map.containsKey(phone_book[i].substring(0,j))) // map안에 있는 phone_book의 접두사를 확인
+                    return answer = false;
+            }
+        }
+        return answer;
+    }
+}
+
+// 해시 함수를 통한 프로그래머스 전화번호 목록 문제이다.
+// level1에서의 마라톤 문제와 같은 맥락을 통해 이해하면 된다.
