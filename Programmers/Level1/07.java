54bfeadd0fdd1f435d953073367298cd3132c222
Byeong Soo Lee <80665963+gloz0315@users.noreply.github.com>
2022-03-07 20:21:36 +0900

    Update 07.java

--- Programmers/Level1/07.java
+++ Programmers/Level1/07.java
@@ -9,30 +9,26 @@ class Solution {
         List<Integer> answerList = new ArrayList<Integer>();
         
         for(int i =0; i < commands.length; i++){
-            for(int j = 0; j < commands[i].length; j++){
-                number[j] = commands[i][j];
-            }
-            
-            answerList.add(sorting(array,commands[i][0],commands[i][1],commands[i][2]));
+            answerList.add(sorting(array,commands[i][0],commands[i][1],commands[i][2]));           // 각각의 입력값들을 answerList에 더해준다.
             
         }
         
         for(int i = 0; i < answerList.size(); i++){
-            answer[i] = answerList.get(i);
+            answer[i] = answerList.get(i);                             // 리스트를 배열에 넣기 위해 for문을 이용하여 넣어준다.
         }
         
         return answer;
     }
     
-    public int sorting(int[] array, int first, int end, int num){
-        List<Integer> numberList = new ArrayList<Integer>();
+    public int sorting(int[] array, int first, int end, int num){       // array리스트를 통해 k번째 수에 관한 값들을 return하는 함수를 만들었다.
+        List<Integer> numberList = new ArrayList<Integer>();            // arraylist를 하나 만들어준다.
         
         for(int i = first-1; i < end; i++){
-            numberList.add(array[i]);
+            numberList.add(array[i]);                                   // array배열에서의 first번째부터 end번째까지! ( 인덱스 번호가 아니다. ) numberList에 넣어준다.
         }
         
-        Collections.sort(numberList);
-        int number = numberList.get(num-1);
+        Collections.sort(numberList);                                   // numberList를 정렬해준다. ( 이때, Collections.sort() 메소드를 이용하여 정렬 )
+        int number = numberList.get(num-1);                             // 내가 찾을 k번째 수를 반환한다. ( 인덱스 번호가 아니다. )
         
         return number;
     }
